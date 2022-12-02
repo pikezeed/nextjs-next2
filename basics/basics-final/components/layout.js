e25f28d4e1cb33b88a375716e3c39e68bee3b84b
pikezeed <pikezeed@users.noreply.github.com>
2022-12-02 15:30:52 +0700

    3 files modified

--- basics/basics-final/components/layout.js
+++ basics/basics-final/components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
+const name = '[Your Name Pike]';
 export const siteTitle = 'Next.js Sample Website From Layout Pike';
 
 export default function Layout({ children, home }) {
